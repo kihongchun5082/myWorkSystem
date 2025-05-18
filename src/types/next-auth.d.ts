195ea28b8cce7b817f33d796328c2d2e23bcd869
전기홍 <chunkihongmacbook@jeongihong-ui-MacBookAir.local>
2025-05-18 21:26:24 +0900

    after first build_20250518

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,21 +1,29 @@
 // import { User } from "@/model/user";
-import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+// import NextAuth, { DefaultSession, DefaultUser } from "next-auth";
+
+// declare module 'next-auth' {
+//  interface Session {
+//   user:{
+//    id: string;
+//    name: string | undefined | null;
+//    username: string;
+//    userId: string;
+//    email: string;
+//    image?: string | null;
+//    createdAt: string;
+//   } & DefaultSession["user"]
+//   company: {
+//    companyName?: string | null;
+//    companyId?: string | null;
+//    image?: string | null;
+//   }
+//  }
+// }
+
+import { AuthUser } from '@/model/user';
 
 declare module 'next-auth' {
- interface Session {
-  user:{
-   id: string;
-   name: string | undefined | null;
-   username: string;
-   userId: string;
-   email: string;
-   image?: string | null;
-   createdAt: string;
-  } & DefaultSession["user"]
-  company: {
-   companyName?: string | null;
-   companyId?: string | null;
-   image?: string | null;
+  interface Session {
+    user: AuthUser;
   }
- }
 }
