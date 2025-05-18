d763333b3f7dd578145c865661b3cd21d1eba1a1
전기홍 <chunkihongmacbook@jeongihong-ui-MacBookAir.local>
2025-05-18 09:51:51 +0900

    마지막커밋이길20250518

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -2,28 +2,27 @@ import { redirect } from "next/navigation"
 import { auth } from "@/auth"
 import SignIn from "@/components/sign-in"
 
-// type Props = {
-//   searchParams: {
-//     callbackUrl: string;
-//   }
-// }
+type Props = {
+  searchParams?: {
+    callbackUrl?: string;
+  }
+}
 // export default async function SignInPage({ searchParams: { callbackUrl },}: Props) {
 
-export default async function SignInPage( context: Promise<{ callbackUrl: string }>) {
+export default async function SignInPage({ searchParams }: Props) {
   
   const session = await auth()
-  const url = (await context).callbackUrl
+  const callbackUrl = searchParams?.callbackUrl || "/"
 
   console.log('session_SignInPage: ',session?.user?.name)
   
   if (session) {
-    redirect('/signin')
-    return;
+    redirect(callbackUrl)
   }
 
   return (
     <section className=" flex justify-center mt-24">
-      <SignIn callbackUrl={url ?? '/'} />
+      <SignIn callbackUrl={ callbackUrl } />
     </section>
   )
 }
